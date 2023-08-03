f37526a047c0e556237f00c329eb183fd88649c7
Lee <leecocky@hotmail.com>
2023-08-03 14:55:29 +0200

    fix input

--- script.js
+++ script.js
@@ -3,10 +3,28 @@ const emailValuetype = document.getElementById('emailValuetype');
 const successText = document.querySelector('.successText')
 const wrapper = document.querySelector('.wrapper');
 const success_page = document.querySelector('.success_page');
-test.addEventListener('click',(e)=>{
-e.preventDefault();
-let input = emailValuetype.value;
-successText.innerHTML=`A confirmation email has been sent to <strong>${input}</strong> Please open it and click the button inside to confirm your subscription.`
-wrapper.style.display='none'
-success_page.style.display='flex'
-})
+
+const emailError = document.querySelector('.emailError')
+const emailInputEl = document.getElementById('emailValuetype');
+test.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const email = emailInputEl.value;
+
+
+   var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (email.match(validRegex)) {
+
+  successText.innerHTML = `A confirmation email has been sent to <strong>${email}</strong> Please open it and click the button inside to confirm your subscription.`;
+  wrapper.style.display = 'none';
+  success_page.style.display = 'flex';
+
+}
+else {
+  // example of showing error
+  const someErrorEl = document.querySelector(".someErrorEl");
+  someErrorEl.style.display = "inline"
+
+  return;
+}
+});
